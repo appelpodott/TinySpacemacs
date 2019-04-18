f9badaeb786e9f66dbae3105330115023889fa5c
Sascha <sascha@rosbach-villmar.de>
2019-04-18 17:31:54 +0000

    Forked from willbush

--- win/mklinks.bat
+++
@@ -1,7 +0,0 @@
-set scriptPath=%~dp0
-
-mklink /J %USERPROFILE%\.emacs.d %scriptPath%..\stow\.emacs.d
-mklink %USERPROFILE%\.gitconfig %scriptPath%\config\.gitconfig
-mklink %USERPROFILE%\.vsvimrc %scriptPath%\config\.vsvimrc
-
-pause
